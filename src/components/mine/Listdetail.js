45fab28d977ecb98c90ade1913568a8043fd304a
jiaxingjun <jiaxingjun0907@163.com>
2019-12-05 09:55:06 +0800

    add listdetailState

--- src/components/mine/Listdetail.js
+++ src/components/mine/Listdetail.js
@@ -3,13 +3,29 @@ import '../../assets/css/mine/listdetail.scss';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import actionCreator from '../../store/actionCreator/mine';
+import actionCreator from '../../store/actionCreator/mine/Listdetail';
 
 class Listdetail extends React.Component {
     render(){
         return(
             <div>
-                lihaha
+               <div className="songsList" style={{ display: this.props.show ? "block" : "none" }}>
+                    {this.props.playlist ? this.props.playlist.map((v, index) => (
+                        <Fragment key={index}>
+                           <div>
+                               <span></span>
+                               <div>
+
+                               </div>
+                               <i></i>
+                               <i></i>
+                           </div>
+                        </Fragment>
+                    )) : <div>haha</div>}
+                </div>
+                <div>
+                    我是你爸爸
+                </div>
             </div>
         )
     }
@@ -21,10 +37,10 @@ class Listdetail extends React.Component {
 
 function mapStateToProps({ mineState}) {
     return {
-        
         playlist: mineState.playlist,
         tracks:mineState.tracks,//单个歌单，歌曲信息
-        toSinglelist:mineState.toSinglelist,//单个歌单，歌曲列表页开关
+        toSinglelist:mineState.toSinglelist,//单个歌单，歌曲列表页开关，
+        toAddsongs:mineState.toAddsongs,//添加歌曲开关
 
     }
 }
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import loginState from './login';
 import searchState from './search';
 import mineState from './mine';
-import discoverReducer from "./discover"
+import discoverReducer from "./discover";
+import listdetailState from './mine/listdetail';
 export default combineReducers({
     loginState,
     searchState,
     mineState,
-    discoverReducer
+    discoverReducer,
+    listdetailState
 })
